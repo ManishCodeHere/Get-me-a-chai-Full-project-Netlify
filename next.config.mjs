5db5ba1ce75214a280e46182ce80787a8bbcfaa8
ManishCodeHere <manishrajak896977@gmail.com>
2026-03-22 20:54:38 +0530

    next config mjs changed

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  devIndicators: {
-    buildActivity: false, // removes the "N" icon
-    appIsrStatus: false,  // removes ISR status dot if shown
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
